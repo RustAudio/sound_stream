49e1740d28429aeff4922c7e5f0f03289c9b2b15
mitchmindtree <mitchell.nordine@gmail.com>
2015-03-08 00:36:58 +1100

    Added optional custom update rate, made custom stream settings optional, added new advanced example, close sound_stream on drop, incremented crate version

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 
-#![feature(core, unboxed_closures)]
+#![feature(collections, core, unboxed_closures, unsafe_destructor)]
 
 extern crate portaudio;
 extern crate sample;
 extern crate time;
 
-pub use buffer::AudioBuffer;
 pub use error::Error;
 pub use event::SoundStream;
 pub use event::Event;
@@ -13,7 +12,6 @@ pub use portaudio::pa::Sample as PaSample;
 pub use sample::{Amplitude, Sample, Wave};
 pub use settings::Settings;
 
-pub mod buffer;
 pub mod error;
 pub mod event;
 pub mod settings;
